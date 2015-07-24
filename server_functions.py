dccefb71afd9a9d1b3d56f8a17305da3bf6b9b75
Arkadionos <madscience_7@hotmail.com>
2015-07-24 16:52:35 -0400

    Initial commit
    Initial commit of project files

---
+++ server_functions.py
@@ -0,0 +1,20 @@
+import datetime
+import mysql.connector
+import collections
+
+with open("updatelog.txt", "a") as logfile:
+    logfile.write("Log at " + str(datetime.datetime.now()) + ":\n")
+
+    #get list of possible notifications
+
+
+
+
+def update_notification(id, testvalue):
+    pass
+
+
+
+    
+
+
